c02497258d315633ba031782f58883d646554ef5
sotirioskentros-bsc <sotirios.kentros@bsc.es>
2026-01-30 18:00:16 +0200

    Delete pages directory

--- pages/js/load.js
+++
@@ -1,7 +0,0 @@
-async function loadJSON(path) {
-  const response = await fetch(path);
-  if (!response.ok) {
-    throw new Error(`Failed to load ${path}: ${response.status}`);
-  }
-  return response.json();
-}
